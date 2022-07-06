ae2a08661f396c813d5144dec1879dca6bec4310
Zhenya <ruban.z1@mail.ru>
2022-07-06 15:29:36 +0300

    Всплытие окна на 3 странице

--- dist/script.js
+++ dist/script.js
@@ -131,6 +131,21 @@ class Slider {
       this.slideIndex = this.slides.length;
     }
 
+    try {
+      //Всплытие окна на 3 странице
+      this.hanson.style.opacity = '0';
+
+      if (n === 3) {
+        this.hanson.classList.add('animated');
+        setTimeout(() => {
+          this.hanson.style.opacity = '1';
+          this.hanson.classList.add('fadeInUp');
+        }, 3000);
+      } else {
+        this.hanson.classList.remove('fadeInUp');
+      }
+    } catch (e) {}
+
     this.slides.forEach(slide => {
       slide.style.display = 'none';
       slide.classList.add('animated', 'fadeInDown');
@@ -143,6 +158,10 @@ class Slider {
   }
 
   render() {
+    try {
+      this.hanson = document.querySelector('.hanson');
+    } catch (e) {}
+
     this.btns.forEach(item => {
       item.addEventListener('click', () => {
         this.plusSlides(1);
